e5d0ddda2f61a4f9006d48e3ea305c38703de86a
qizengqwq <zengqiqwq@gmail.com>
2025-11-29 02:14:09 -0500

    Add files via upload
    connection.py using in prompt_citation_chat.ipynb

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,17 +1,21 @@
+# backend/db/connection.py
+
 import os
 from dotenv import load_dotenv
 import mysql.connector
 
-#loads the environment variables from a .env file
+# load .env
 load_dotenv()
 
 def get_db_connection():
-    """Establishes and returns a connection to the database using environment variables."""
-    connection = mysql.connector.connect(
+    """
+    create and return a MySQL connection
+    """
+    conn = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT", "3306"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME", "thalia"),  # keep same with run_schema.py
     )
-    return connection
+    return conn
